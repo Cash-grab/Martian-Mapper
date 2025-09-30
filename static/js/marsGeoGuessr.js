085273c6575c28e96162c2a1a01de99cf7c9d4ac
Cash-grab <aarushdwivedi21092005@gmail.com>
2025-09-30 14:45:21 +0530

    Making minor improvements, added link to acess the Mars map route

--- static/js/marsGeoGuessr.js
+++ static/js/marsGeoGuessr.js
@@ -29,7 +29,7 @@ async function fetchRandomMarsImage() {
 
 // --- Start a new game round ---
 async function startRound() {
-    document.getElementById('game-info').textContent = "Loading image...";
+    document.getElementById('game-info').textContent = "Loading image Please be paitent!...";
     document.getElementById('next-round').style.display = "none";
     if (guessMarker) { map.removeLayer(guessMarker); }
     if (answerMarker) { map.removeLayer(answerMarker); }
@@ -79,7 +79,77 @@ map.on('click', function(e) {
 });
 
 // --- Next round button handler ---
+
 document.getElementById('next-round').onclick = startRound;
+
+
+
+
+function makeDraggable(element, dragHandle) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  if (dragHandle) {
+    // If a drag handle is provided, listen for mousedown on the handle
+    dragHandle.onmousedown = dragMouseDown;
+  } else {
+    // Otherwise, the entire element is draggable
+    element.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault(); // Prevent default browser drag behavior
+
+    // Get the mouse cursor position at startup
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    document.onmouseup = closeDragElement; // Stop dragging when mouse button is released
+    document.onmousemove = elementDrag;   // Call a function whenever the cursor moves
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    // Calculate the new cursor position
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    // Set the element's new position
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // Stop moving when mouse button is released
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Get the draggable div and its header
+const draggableDiv = document.getElementById("gameUI");
+const draggableDivHeader = document.getElementById("gameUIHeader");
+
+// Make the div draggable using its header as the handle
+makeDraggable(draggableDiv, draggableDivHeader);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // --- Start the first round when the page loads ---
 startRound();
